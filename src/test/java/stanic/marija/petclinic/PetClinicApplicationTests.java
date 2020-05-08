e561837700b8f7b54a642f82c3f4e2f7fd965d97
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-08 11:59:05 +0100

    Initial project add

---
+++ src/test/java/stanic/marija/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package stanic.marija.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
