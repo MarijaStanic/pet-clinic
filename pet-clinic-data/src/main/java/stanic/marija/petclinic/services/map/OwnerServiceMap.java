4b1dd020dbe159a2040e24fc768429dfd2781948
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-12 21:12:51 +0100

    Add bootstrap for data

--- pet-clinic-data/src/main/java/stanic/marija/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/stanic/marija/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package stanic.marija.petclinic.services.map;
 
 import stanic.marija.petclinic.model.Owner;
-import stanic.marija.petclinic.services.CrudService;
+import stanic.marija.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -30,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
