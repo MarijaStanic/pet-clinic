7a16c20472416ebbd4f45fdb06357de8f08e077c
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-16 11:43:09 +0100

    Update base entity with JPA annotations

--- pet-clinic-data/src/main/java/stanic/marija/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/stanic/marija/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package stanic.marija.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // We don't need this class mapped to the database
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
