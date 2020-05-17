abdeade698c30431f87630b4b9fda225565cc07d
Marija Stanic <marija.stanic@hotmail.ca>
2020-05-17 09:24:32 +0100

    Add project Lombok to project

--- pet-clinic-data/src/main/java/stanic/marija/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/stanic/marija/petclinic/model/Speciality.java
@@ -1,17 +1,17 @@
 package stanic.marija.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Speciality extends BaseEntity {
 
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
